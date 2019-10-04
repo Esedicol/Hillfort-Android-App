9275055a53e55a97d143abf0437d15ccfcd4a5ef
esedicol <sedicolemmanuel@gmail.com>
2019-10-04 13:23:26 +0100

    Added a new dependecies and imported AnkoLogger

--- Lab_01/app/src/main/java/org/wit/placemark/PlacemarkActivity.kt
+++ Lab_01/app/src/main/java/org/wit/placemark/PlacemarkActivity.kt
@@ -2,11 +2,12 @@ package org.wit.placemark
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.AnkoLogger
 
-class MainActivity : AppCompatActivity() {
+class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_placemark)
     }
 }
