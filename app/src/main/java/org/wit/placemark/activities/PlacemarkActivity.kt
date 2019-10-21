3bd7ad61e2083e9f01307ea12ab66e474f1ede4e
Emmanuel Sedicol <sedicolemmanuel@gmail.com>
2019-10-21 22:51:00 +0100

    Implemented nullable and non-null types

--- app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -1,41 +1,40 @@
 package org.wit.placemark.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 import org.wit.placemark.R
+import org.wit.placemark.main.MainApp
 import org.wit.placemark.models.PlacemarkModel
 
-class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
 
-    val placemarks = ArrayList<PlacemarkModel>()
+class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
+    var app : MainApp? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        app = application as MainApp
 
-        info("Placemark Activity started..")
         btnAdd.setOnClickListener() {
             val title = placemarkTitle.text.toString()
             val description = description.text.toString()
 
+            val pm = PlacemarkModel(title, description)
+
             if (title.isNotEmpty() || description.isNotEmpty()) {
-                info("Add Button Pressed")
-
-                val pm = PlacemarkModel(title, description)
-                placemarks.add(pm)
-
-                for(i in placemarks.indices) {
-                    println("Placemark[$i]: Title: ${placemarks[i].title}, Description: ${placemarks[i].description} ${this.placemarks[i]}")
+                app!!.placemarks.add(pm)
+                info("add Button Pressed: ${pm}")
+                for (i in app!!.placemarks.indices) {
+                    println("Placemark[$i]: Title: ${app!!.placemarks[i].title}, Description: ${app!!.placemarks[i].description} ${this.app!!.placemarks[i]}")
                 }
             }
             else {
-               toast("Please Enter a title")
-
+                toast ("Please Enter a title")
             }
         }
     }
--- app/src/main/java/org/wit/placemark/main/MainApp.kt
+++ app/src/main/java/org/wit/placemark/main/MainApp.kt
@@ -4,8 +4,10 @@ package org.wit.placemark.main
 import android.app.Application
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.wit.placemark.models.PlacemarkModel
 
 class MainApp : Application(), AnkoLogger {
+    val placemarks = ArrayList<PlacemarkModel>()
 
     override fun onCreate() {
         super.onCreate()
