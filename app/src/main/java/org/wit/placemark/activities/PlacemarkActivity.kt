ff2ce69c623af7e43e93b59688af98a5df90d3a9
Emmanuel Sedicol <sedicolemmanuel@gmail.com>
2019-10-22 10:08:22 +0100

    Got rid or nullables by using 'lateinit'

--- app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.AnkoLogger
-import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 import org.wit.placemark.R
 import org.wit.placemark.main.MainApp
@@ -12,25 +11,24 @@ import org.wit.placemark.models.PlacemarkModel
 
 
 class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
-    var app : MainApp? = null
+    lateinit var app : MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         app = application as MainApp
 
-        btnAdd.setOnClickListener() {
+        btnAdd.setOnClickListener {
             val title = placemarkTitle.text.toString()
             val description = description.text.toString()
 
             val pm = PlacemarkModel(title, description)
 
             if (title.isNotEmpty() || description.isNotEmpty()) {
-                app!!.placemarks.add(pm)
-                info("add Button Pressed: ${pm}")
-                for (i in app!!.placemarks.indices) {
-                    println("Placemark[$i]: Title: ${app!!.placemarks[i].title}, Description: ${app!!.placemarks[i].description} ${this.app!!.placemarks[i]}")
+                app.placemarks.add(pm)
+                toast("Adding New Placemark: ${pm.title}")
+                for (i in app.placemarks.indices) {
+                    println("Placemark[$i]: Title: ${app.placemarks[i].title}, Description: ${app.placemarks[i].description} ${this.app.placemarks[i]}")
                 }
             }
             else {
