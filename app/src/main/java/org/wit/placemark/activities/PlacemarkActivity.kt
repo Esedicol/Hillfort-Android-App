5edbf1dcf0415e773497c3f8533cdef0df9da8c6
Emmanuel Sedicol <sedicolemmanuel@gmail.com>
2019-10-28 15:57:39 +0000

    Added "save" on toolbar and remove the button "Add Placemark"

--- app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -8,12 +8,14 @@ import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_placemark.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.jetbrains.anko.intentFor
 import org.jetbrains.anko.toast
 import org.wit.placemark.R
 import org.wit.placemark.helpers.readImage
 import org.wit.placemark.helpers.readImageFromPath
 import org.wit.placemark.helpers.showImagePicker
 import org.wit.placemark.main.MainApp
+import org.wit.placemark.models.Location
 import org.wit.placemark.models.PlacemarkModel
 
 class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
@@ -21,16 +23,18 @@ class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
     var placemark = PlacemarkModel()
     lateinit var app: MainApp
     val IMAGE_REQUEST = 1
+    val LOCATION_REQUEST = 2
+    var location = Location(52.245696, -7.139102, 15f)
+    var edit = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_placemark)
-
         toolbarAdd.title = title
         setSupportActionBar(toolbarAdd)
+        info("Placemark Activity started..")
 
         app = application as MainApp
-        var edit = false
 
         if (intent.hasExtra("placemark_edit")) {
             edit = true
@@ -39,30 +43,14 @@ class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
             placemarkTitle.setText(placemark.title)
             description.setText(placemark.description)
             placemarkImage.setImageBitmap(readImageFromPath(this, placemark.image))
-
-            btnAdd.setText(R.string.save_placemark)
-        }
-
-        btnAdd.setOnClickListener() {
-            placemark.title = placemarkTitle.text.toString()
-            placemark.description = description.text.toString()
-
-            if (placemark.title.isEmpty()) {
-                toast(R.string.enter_placemark_title)
-            } else {
-                if (edit) {
-                    app.placemarks.update(placemark.copy())
-                } else {
-                    app.placemarks.create(placemark.copy())
-                }
-            }
-            info("add Button Pressed: $placemarkTitle")
-            setResult(AppCompatActivity.RESULT_OK)
-            finish()
         }
 
         chooseImage.setOnClickListener {
             showImagePicker(this, IMAGE_REQUEST)
+        }
+
+        placemarkLocation.setOnClickListener {
+            startActivityForResult(intentFor<MapActivity>().putExtra("location", location), LOCATION_REQUEST)
         }
     }
 
@@ -74,8 +62,24 @@ class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         when (item?.itemId) {
             R.id.item_cancel -> finish()
+            R.id.logout -> toast("Loggin out")
             R.id.login -> toast("Logging In")
-            R.id.logout -> toast("Logging Out")
+            R.id.btnAdd -> {
+                placemark.title = placemarkTitle.text.toString()
+                placemark.description = description.text.toString()
+
+                if (placemark.title.isEmpty()) {
+                    toast(R.string.enter_placemark_title)
+                } else {
+                    if (edit) {
+                        app.placemarks.update(placemark.copy())
+                    } else {
+                        app.placemarks.create(placemark.copy())
+                    }
+                }
+                setResult(AppCompatActivity.RESULT_OK)
+                finish()
+            }
         }
         return super.onOptionsItemSelected(item)
     }
@@ -87,8 +91,15 @@ class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
                 if (data != null) {
                     placemark.image = data.getData().toString()
                     placemarkImage.setImageBitmap(readImage(this, resultCode, data))
+                    chooseImage.setText(R.string.change_placemark_image)
+                }
+            }
+            LOCATION_REQUEST -> {
+                if (data != null) {
+                    location = data.extras?.getParcelable<Location>("location")!!
                 }
             }
         }
     }
 }
+
